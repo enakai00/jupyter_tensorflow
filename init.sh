6b5fee0970fab035ccecc40f942eba973adc0d82
Etsuji Nakai <enakai@redhat.com>
2016-03-29 09:26:29 +0900

    Add -q to grep

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if [ "${PASSWORD-undef}" = "undef" ]; then
   export PASSWORD='passw0rd'
 fi
 
-if ! grep -E '^c.NotebookApp.password =' $HOME/.jupyter/jupyter_notebook_config.py; then
+if ! grep -qE '^c.NotebookApp.password =' $HOME/.jupyter/jupyter_notebook_config.py; then
   HASH=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
   echo "c.NotebookApp.password = u'${HASH}'" >>$HOME/.jupyter/jupyter_notebook_config.py
 fi
